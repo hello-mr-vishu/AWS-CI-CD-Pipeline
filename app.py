15fe4d300a37362c77850ff2b676f035ae5be943
hello-mr-vishu <vishnurongali21@gmail.com>
2025-05-20 17:20:10 +0530

    ec2

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template, request
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 from src.exception import CustomException
 
+
 application = Flask(__name__)
 app = application
 
